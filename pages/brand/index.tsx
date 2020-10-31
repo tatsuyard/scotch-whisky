57f0e253e2aeb2e932773279872fedd1d64eb0ed
tatsuya kamikawara <tatsuyardful@gmail.com>
2020-10-31 09:27:07 +0900

    add text

--- pages/brand/index.tsx
+++ pages/brand/index.tsx
@@ -15,17 +15,23 @@ const Home: React.FC = () => (
 
       <a href="#" className={styles.card}>
         <h3>ハイランド &rarr;</h3>
-        <p>Learn about Next.js in an interactive course with quizzes!</p>
+        <p>
+          スコットランドの北側にある、広大な地域のハイランド。スコットランドの蒸溜所は、3分の1ほどがハイランドにあります。広いハイランドのなかでも、有名な蒸溜所が多くあるのが北ハイランドです。
+        </p>
       </a>
 
       <a href="#" className={styles.card}>
         <h3>スペイサイド &rarr;</h3>
-        <p>Discover and deploy boilerplate example Next.js projects.</p>
+        <p>
+          さほど大きな土地ではありませんが、大麦の収穫量が多いため多数の蒸溜所があり、有名な蒸溜所が集中しているスペイサイド。ウイスキーは、花やハチミツのようなフローラルな香りと華やかな風味の銘柄が多く、親しみやすいのが特徴です。
+        </p>
       </a>
 
       <a href="#" className={styles.card}>
         <h3>ローランド &rarr;</h3>
-        <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+        <p>
+          ローランドは、スコットランドの南側に位置する地方です。かつてはたくさんのモルトウイスキー蒸溜所がありましたが、次第に衰退し、現在稼働している蒸溜所は6つのみとなっています。
+        </p>
       </a>
     </div>
   </div>
