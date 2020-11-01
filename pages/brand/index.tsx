9f441a8bc6f7dc7c7996a3385026876e5a05ebc5
tatsuya kamikawara <tatsuyardful@gmail.com>
2020-11-01 10:48:05 +0900

    import header to brand index

--- pages/brand/index.tsx
+++ pages/brand/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import styles from "../../styles/Home.module.css";
+import Header from "../../components/Header";
 
 const Home: React.FC = () => (
   <div className={styles.container}>
+    <div>
+      <Header />
+    </div>
     <h1 className={styles.title}>銘柄一覧</h1>
 
     <div className={styles.grid}>
