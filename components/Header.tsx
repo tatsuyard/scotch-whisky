03094fa77ce4aaa3f1a8f4d99b2a208f27af8c3d
tatsuya kamikawara <tatsuyardful@gmail.com>
2020-11-04 15:36:00 +0900

    rename header css

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
-import styles from "../styles/Common.module.css";
+import styles from "../styles/Header.module.css";
 
 const linkStyle = {
   marginRight: 15,
 };
 
 const Header = () => (
-  <div className={styles.main}>
+  <div className={styles.header}>
     <div>
       <Link href="/">
         <a style={linkStyle}>Home</a>
