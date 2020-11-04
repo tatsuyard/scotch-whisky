26901b2299194c4c1e6224599661d7760642ff9a
tatsuya kamikawara <tatsuyardful@gmail.com>
2020-11-04 15:25:27 +0900

    add styl

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,20 @@
 import Link from "next/link";
+import styles from "../styles/Common.module.css";
 
 const linkStyle = {
   marginRight: 15,
 };
 
 const Header = () => (
-  <div>
-    <Link href="/">
-      <a style={linkStyle}>Home</a>
-    </Link>
-    <Link href="/brand">
-      <a style={linkStyle}>Brand</a>
-    </Link>
+  <div className={styles.main}>
+    <div>
+      <Link href="/">
+        <a style={linkStyle}>Home</a>
+      </Link>
+      <Link href="/brand">
+        <a style={linkStyle}>Brand</a>
+      </Link>
+    </div>
   </div>
 );
 
