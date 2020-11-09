815ae189352409b65f5f304bffbfc7c379335ca7
tatsuya kamikawara <tatsuyardful@gmail.com>
2020-11-09 15:55:06 +0900

    tmp

--- pages/brand/index.tsx
+++ pages/brand/index.tsx
@@ -1,36 +1,65 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import styles from "../../styles/Home.module.css";
 import Header from "../../components/Header";
 
-const Home: React.FC = () => (
-  <div className={styles.container}>
-    <div>
-      <Header />
+const Home: React.FC<Props> = (props) => {
+  const { db } = props;
+  const [brands] = useState([]);
+  const collection = useMemo(() => {
+    const col = db
+      .collection("brands")
+      .get()
+      .then((snapshot) => {
+        let data = [];
+        snapshot.forEach((doc) => {
+          data.push(
+            Object.assign(
+              {
+                id: doc.id,
+              },
+              doc.data()
+            )
+          );
+        });
+        return data;
+      })
+      .catch((error) => {
+        return [];
+      });
+    return col;
+  }, []);
+
+  console.log(collection);
+  return (
+    <div className={styles.container}>
+      <div>
+        <Header />
+      </div>
+      <h1 className={styles.title}>銘柄一覧</h1>
+
+      <div className={styles.grid}>
+        <a href="#" className={styles.card}>
+          <h3>アイラ &rarr;</h3>
+          <p>アイラについて</p>
+        </a>
+
+        <a href="#" className={styles.card}>
+          <h3>ハイランド &rarr;</h3>
+          <p>ハイランドについて</p>
+        </a>
+
+        <a href="#" className={styles.card}>
+          <h3>スペイサイド &rarr;</h3>
+          <p>スペイサイドについて</p>
+        </a>
+
+        <a href="#" className={styles.card}>
+          <h3>ローランド &rarr;</h3>
+          <p>ローランドについて</p>
+        </a>
+      </div>
     </div>
-    <h1 className={styles.title}>銘柄一覧</h1>
-
-    <div className={styles.grid}>
-      <a href="#" className={styles.card}>
-        <h3>アイラ &rarr;</h3>
-        <p>アイラについて</p>
-      </a>
-
-      <a href="#" className={styles.card}>
-        <h3>ハイランド &rarr;</h3>
-        <p>ハイランドについて</p>
-      </a>
-
-      <a href="#" className={styles.card}>
-        <h3>スペイサイド &rarr;</h3>
-        <p>スペイサイドについて</p>
-      </a>
-
-      <a href="#" className={styles.card}>
-        <h3>ローランド &rarr;</h3>
-        <p>ローランドについて</p>
-      </a>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Home;
