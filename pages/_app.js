103e326ccee3fc3bb4555d8f24b18d5c31058a0d
tatsuya kamikawara <tatsuyardful@gmail.com>
2020-11-09 15:54:42 +0900

    add firestore

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import firebase from "firebase/app";
+import initFirebase from "../firebase/init";
+
+initFirebase();
+const db = firebase.firestore();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const params = { db };
+  return <Component {...pageProps} {...params} />;
 }
 
-export default MyApp
+export default MyApp;
