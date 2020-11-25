077fcc3a72bb3f7d3117de27534deb7259f69727
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2020-11-25 16:16:30 +0900

    Headerを修正

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,39 @@
+import { FC, useContext } from 'react';
 import Link from "next/link";
+import { AuthContext } from './Auth';
 import styles from "../styles/Header.module.css";
 
 const linkStyle = {
   marginRight: 15,
 };
 
-const Header = () => (
-  <div className={styles.header}>
-    <div>
-      <Link href="/">
-        <a style={linkStyle}>Home</a>
+const Header: FC = () => {
+  const { currentUser } = useContext(AuthContext);
+
+  const SignInLink = () => (
+    !currentUser && (
+      <Link href="/signin">
+        <a style={linkStyle}>SignIn</a>
       </Link>
-      <Link href="/brand">
-        <a style={linkStyle}>Brand</a>
-      </Link>
-      <Link href="/about">
-        <a style={linkStyle}>About</a>
-      </Link>
+    )
+  )
+  return (
+    <div className={styles.header}>
+      <div>
+        <Link href="/">
+          <a style={linkStyle}>Home</a>
+        </Link>
+        <Link href="/brand">
+          <a style={linkStyle}>Brand</a>
+        </Link>
+        <Link href="/about">
+          <a style={linkStyle}>About</a>
+        </Link>
+        <SignInLink />
+      </div>
     </div>
-  </div>
-);
+  );    
+}
+  
 
 export default Header;
