990cdfd231e58f2f4e9db30aeac7eaf562c705c5
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2020-12-30 07:36:36 +0900

    style変更

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,26 @@
 import Link from "next/link";
 
 const Header = () => (
-  <div className="">
-    <ul className="flex">
-      <li className="mr-6">
+  <div className="container mx-auto bg-purple-300 p-5">
+    <nav class="flex justify-between">
+    <ul className="flex flex-row">
+      <li className="pr-5">
         <Link href="/">
-          <a className="text-blue-500 hover:text-blue-800">Home</a>
+          <a className="text-blue-500 hover:text-blue-800 font-medium">Home</a>
         </Link>
       </li>
-      <li className="mr-6">
+      <li className="pr-5">
         <Link href="/brand">
           <a className="text-blue-500 hover:text-blue-800">Brand</a>
         </Link>
       </li>
-      <li className="mr-6">
+      <li className="pr-5">
         <Link href="/about">
           <a className="text-blue-500 hover:text-blue-800">About</a>
         </Link>
       </li>
     </ul>
+    </nav>
   </div>
 );
 
