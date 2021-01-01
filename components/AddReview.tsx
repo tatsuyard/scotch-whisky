01b27c5ca0a1375d6c4d5bcc8846274ddaadbd07
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-01-02 07:08:24 +0900

    tmp

--- components/AddReview.tsx
+++ components/AddReview.tsx
@@ -1,2 +1,54 @@
 import React, { useState } from "react";
 
+const AddReview: React.FC = () => {
+
+    const [title, setTitle] = useState('')
+    const [review, setReview] = useState('')
+    
+
+    const handleSubmit = () => {
+        setTitle('')
+        setReview('')
+        try {
+            console.log('add');
+        } catch (error) {
+            alert(error)
+            // setPending(false);
+        }
+    }
+
+    return (
+        
+          <div>
+            <h2>
+              <label>
+                Add Review
+              </label>
+            </h2>
+            <label>
+              Title
+            </label>
+              <input
+                type="text"
+                name="title"
+                autoComplete="off"
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+            />
+            <label>
+              Review
+            </label>
+            <input
+              type="text"
+              name="review"
+              value={review} 
+              onChange={e => setReview(e.target.value)}
+            />
+            <button className="btn-blue" onClick={handleSubmit} disabled={!( title && review)}>
+              Post
+            </button>
+          </div>
+    
+    )
+}
+export default AddReview
