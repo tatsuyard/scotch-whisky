ab9bf168571e54511e95e9de862f79ebbcaabcf5
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-01-02 07:24:19 +0900

    indent

--- components/AddReview.tsx
+++ components/AddReview.tsx
@@ -18,39 +18,37 @@ const AddReview: React.FC = () => {
     }
 
     return (
-        
-          <div>
-            <h2>
-              <label>
-                Add Review
-              </label>
-            </h2>
-            <label>
-              Title
-            </label>
-            <input
-                type="text"
-                name="title"
-                autoComplete="off"
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-                className="p-2 border"
-            />
-            <label>
-              Review
-            </label>
-            <input
-              type="text"
-              name="review"
-              value={review} 
-              onChange={e => setReview(e.target.value)}
-              className="p-4 border"
-            />
-            <button className="btn-blue" onClick={handleSubmit} disabled={!( title && review)}>
-              Post
-            </button>
-          </div>
-    
+      <div>
+        <h2>
+          <label>
+            Add Review
+          </label>
+        </h2>
+        <label>
+          Title
+        </label>
+        <input
+            type="text"
+            name="title"
+            autoComplete="off"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            className="p-2 border"
+        />
+        <label>
+          Review
+        </label>
+        <input
+          type="text"
+          name="review"
+          value={review} 
+          onChange={e => setReview(e.target.value)}
+          className="p-4 border"
+        />
+        <button className="btn-blue" onClick={handleSubmit} disabled={!( title && review)}>
+          Post
+        </button>
+      </div>
     )
 }
 export default AddReview
