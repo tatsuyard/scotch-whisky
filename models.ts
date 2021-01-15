65b8aa007752b19de49355029b6650eed02c8227
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-01-15 19:14:42 +0900

    type追加

--- models.ts
+++ models.ts
@@ -1,5 +1,16 @@
+import firebase from "firebase/app";
+
 export type Brand = {
-    id?: string  //ドキュメント識別用ID(自動採番)
-    name: string
-    description: string | null
+    id?: string,  //ドキュメント識別用ID(自動採番)
+    name: string,
+    description: string,
 }
+
+export type Review ={
+    id?: string,
+    title: string,
+    comment: string,
+    brand: firebase.DocumentReference,
+    createdAt: firebase.firestore.FieldValue | null
+  }
+  
