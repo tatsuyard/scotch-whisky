0add9d1612a5b4b3bc4f718f741747b293cd9e59
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-01-18 07:07:02 +0900

    李ファクタ

--- pages/brand/new.tsx
+++ pages/brand/new.tsx
@@ -4,66 +4,64 @@ import Header from "../../components/Header";
 import initFirebase from "../../firebase/init";
 import firebase from "firebase/app";
 import { Collection } from "../../consts";
+import { Brand } from "../../models"
 
 initFirebase();
 const db = firebase.firestore();
 
-const New: React.FC = () => {
-    const [name, setName] = useState('')
-    const [desc, setDesc] = useState('')
-    
 
-    const handleSubmit = async () => {
-        setName('')
-        setDesc('')
-        try {
-            db.collection(Collection.brands).add({
-                name: name,
-                description: desc
-            })
-        } catch (error) {
-            alert(error)
-            // setPending(false);
-        }
+const New: React.FC = () => {
+  const [brand, setBrand] = useState<Brand>({ name: '', description: '' })
+    
+  const handleSubmit = async () => {
+    try {
+      db.collection(Collection.brands).add({      
+        name: brand.name,
+        description: brand.description
+      })
     }
+    catch (error) {
+      alert(error)
+    }
+  }
 
     return (
-        <div className={styles.container}>
-          <div>
-              <Header />
-          </div>
-          <div>
-            <h2>
-              <label>
-                Add Brand 
-              </label>
-            </h2>
-            <label>
-              name
-            </label>
-              <input
-                type="text"
-                name="name"
-                autoComplete="off"
-                value={name}
-                onChange={e => setName(e.target.value)}
-                className="p-2 border"
-            />
-            <label>
-              description
-            </label>
-            <input
-              type="text"
-              name="description"
-              value={desc} 
-              onChange={e => setDesc(e.target.value)}
-              className="p-4 border"
-            />
-            <button className="btn-blue" onClick={handleSubmit} disabled={!(name && desc)}>
-              Add
-            </button>
-          </div>
+      <div className={styles.container}>
+        <div>
+          <Header />
         </div>
+        <div>
+          <h2>
+            <label>              
+              Add Brand 
+            </label>
+          </h2>
+          <label>  
+            name
+          </label>
+          <input
+            type="text"
+            name="name"
+            autoComplete="off"
+            value={brand.name}
+            onChange={e => setBrand({...brand, name: e.target.value})}
+            className="p-2 border"
+          />
+          <label>
+            description   
+          </label>
+          <input
+            type="text"
+            name="description"
+            value={brand.description} 
+            onChange={e => setBrand({...brand, description: e.target.value})}
+            className="p-4 border"
+          />
+          <button className="btn-blue" onClick={handleSubmit} disabled={!(brand.name && brand.description)}>
+            Add
+          </button>
+        </div>
+      </div>
     )
 }
 export default New
