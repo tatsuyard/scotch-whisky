596c11ca2b9cee80ebce9af7e7551e49acc4a72d
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-01-20 07:22:26 +0900

    tmp

--- pages/brand/[id].tsx
+++ pages/brand/[id].tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../../styles/Home.module.css';
 import Header from '../../components/Header';
 import AddReview from '../../components/AddReview';
 import { useRouter } from 'next/router';
+import initFirebase from "../../firebase/init";
+import firebase from "firebase/app";
+import { Collection } from '../../consts';
+
+initFirebase();
+const db = firebase.firestore();
 
 const Brand: React.FC = () => {
   const router = useRouter();
-  const { name, id } = router.query;
+  const { name, id } = router.query;  
+  const [reviews, setReviews] = useState<[]>([])
+    
 
+  useEffect(async() => {
+      const querySnapshot = await db.collection(Collection.reviews).where('brand', '==', id).get()
+      const data = querySnapshot.docs.map(doc => {
+        return doc.data()
+      });
+      console.log(data)
+      setReviews(data)
+      return () => {
+        querySnapshot
+      }
+  }, [])
+    
   return (
     <div className={styles.container}>
       <div>
@@ -16,8 +36,20 @@ const Brand: React.FC = () => {
 
       <main className={styles.main}>
         <h1 className="title">Brand: {name}</h1>
-        <p className="text-center text-teal-500 text-2xl py-4">This is an {name} detail Page.</p>
-      </main>
+              <p className="text-center text-teal-500 text-2xl py-4">This is an {name} detail Page.</p>
+              <h2>reviews</h2>
+          
+          
+              {
+                  reviews.map(review =>
+                    <div>
+                      <h3>{review.title}</h3>
+                      <p>{review.comment}</p>          
+                   </div>
+                  )}
+              </main>
+          
+          
       <div>
         <AddReview brandId={Array.isArray(id) ? id[0] : id} />
       </div>
