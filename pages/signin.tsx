6d57eed0f4b00d655ff1100a3c14368572b96a04
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-01-27 06:59:09 +0900

    clasNameに変更

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -23,7 +23,7 @@ const SignIn: FC = () => {
             <div className="w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 
             px-6 py-10 sm:px-10 sm:py-6 
             bg-white rounded-lg shadow-md lg:shadow-lg">
-              <h2 class="text-center font-semibold text-3xl lg:text-4xl text-gray-800">
+              <h2 className="text-center font-semibold text-3xl lg:text-4xl text-gray-800">
                 SignIn
               </h2>
               <button className="w-full py-3 mt-10 bg-gray-800 rounded-sm
