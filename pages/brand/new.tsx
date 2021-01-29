04c25c464645763dc9b9a76ff616d76c7fbc6bd0
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-01-30 06:48:12 +0900

    file upload

--- pages/brand/new.tsx
+++ pages/brand/new.tsx
@@ -9,10 +9,23 @@ import { Brand } from "../../models"
 initFirebase();
 const db = firebase.firestore();
 
+type ImageFile = File & {
+  preview: string;
+};
 
 const New: React.FC = () => {
   const [brand, setBrand] = useState<Brand>({ name: '', description: '' })
+  const [file, setFile] = useState<ImageFile[]>({});
     
+  console.log(file.preview)
+
+  const handleImageAsFile = (e) => {
+    const image = e.target.files[0]
+    setFile({
+      preview: URL.createObjectURL(image)
+    })
+  }
+
   const handleSubmit = async () => {
     try {
       await db.collection(Collection.brands).add({      
@@ -57,7 +70,16 @@ const New: React.FC = () => {
             value={brand.description} 
             onChange={e => setBrand({...brand, description: e.target.value})}
             className="shadow appearance-none border border-red rounded w-full py-12 px-9 text-grey-darker mb-3"
-          />
+            />
+          <img src={file && file.preview} />
+          <label className="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white">
+            <svg className="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+            </svg>
+            <span className="mt-2 text-base leading-normal">Select a file</span>
+              <input type='file' class="hidden" onChange={ handleImageAsFile } />
+          </label>
+
           <button className="btn-blue" onClick={handleSubmit} disabled={!(brand.name && brand.description)}>
             Add
           </button>
