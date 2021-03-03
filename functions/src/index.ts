be8acaed5da1ba7951f82f22e16452e6f1a6e079
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-03-04 06:50:00 +0900

    修正

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -28,16 +28,16 @@ function modifyAdmin(uid: string, isAdmin: boolean) {
 
 // Take the text parameter passed to this HTTP endpoint and insert it into
 // Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https
-    .onRequest(async (req, res) => {
-      // Grab the text parameter.
-      const original = req.query.text;
-      // Push the new message into Firestore using the Firebase Admin SDK.
-      const writeResult = await admin.firestore().collection("messages")
-          .add({original: original});
-      // Send back a message that we've successfully written the message
-      res.json({result: `Message with ID: ${writeResult.id} added.`});
-    });
+// exports.addMessage = functions.https
+//     .onRequest(async (req, res) => {
+//       // Grab the text parameter.
+//       const original = req.query.text;
+//       // Push the new message into Firestore using the Firebase Admin SDK.
+//       const writeResult = await admin.firestore().collection("messages")
+//           .add({original: original});
+//       // Send back a message that we've successfully written the message
+//       res.json({result: `Message with ID: ${writeResult.id} added.`});
+//     });
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
