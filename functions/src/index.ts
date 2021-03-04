6b01e9d5e684b03c09930e9951b1e7beb8dd22eb
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-03-05 06:32:41 +0900

    不要な箇所を削除

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -25,23 +25,3 @@ function modifyAdmin(uid: string, isAdmin: boolean) {
     console.log("変更しました");
   });
 }
-
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-// exports.addMessage = functions.https
-//     .onRequest(async (req, res) => {
-//       // Grab the text parameter.
-//       const original = req.query.text;
-//       // Push the new message into Firestore using the Firebase Admin SDK.
-//       const writeResult = await admin.firestore().collection("messages")
-//           .add({original: original});
-//       // Send back a message that we've successfully written the message
-//       res.json({result: `Message with ID: ${writeResult.id} added.`});
-//     });
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
